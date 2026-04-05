095e6d7256b838336b0a4e491132059b914bc9f1
Nemuidere <olokoxthelegend@gmail.com>
2026-04-05 19:12:47 +0200

    chore: changed dev app info

--- src/__init__.py
+++ src/__init__.py
@@ -4,5 +4,5 @@ EasyMacro - GUI Macro Manager for AutoHotkey Scripts
 A Python-based GUI macro manager that requires no coding knowledge from users.
 """
 
-__version__ = "0.1.0"
-__author__ = "EasyMacro Team"
+__version__ = "dev 0.2"
+__author__ = "Nemuidere"
